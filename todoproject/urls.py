a604497994a2e4688ebb248f1c4e66d77b139aba
hamphreytanatswa-collab <hamphreytanatswa@gmail.com>
2026-04-29 20:57:50 +0200

    Prefill deploy login credentials

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -17,10 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from todos.forms import PrefilledAuthenticationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todos.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='todos/login.html'), name='login'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='todos/login.html',
+            authentication_form=PrefilledAuthenticationForm,
+        ),
+        name='login',
+    ),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- todos/forms.py
+++ todos/forms.py
@@ -1,5 +1,25 @@
+import os
+
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
 from .models import Todo
+
+
+class PrefilledAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget.render_value = True
+
+        if not self.is_bound:
+            self.fields['username'].initial = os.environ.get(
+                'DJANGO_SUPERUSER_USERNAME',
+                'admin',
+            )
+            self.fields['password'].initial = os.environ.get(
+                'DJANGO_SUPERUSER_PASSWORD',
+                '',
+            )
 
 class TodoForm(forms.ModelForm):
     class Meta:
